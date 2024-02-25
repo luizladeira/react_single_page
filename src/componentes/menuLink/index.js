4243c5e3646897b5f371dd7dc7a7159d49a413c2
Luiz Paulo Araujo Ladeira <A238349@ae.local>
2024-02-25 13:55:13 -0300

    ultizando o NavLink no lugar o useLocation

--- src/componentes/menuLink/index.js
+++ src/componentes/menuLink/index.js
@@ -1,18 +1,14 @@
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './MenuLink.module.css';
 
 export default function MenuLink({children, to}){
 
-    const localizacao = useLocation();
-
     return (
-        <Link className={`
-            ${styles.link} 
-            ${localizacao.pathname === to ? styles.linkDestacado : ""}
-        `} to={to}>
+        <NavLink className={({isActive}) => 
+            `${styles.link}
+             ${isActive ? styles.linkDestacado : ""}
+            `} to={to}>
             {children}
-        </Link>
-              
-        
+        </NavLink>
     )
 }
